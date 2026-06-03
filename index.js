36917b2bc4f5bb1fabe65f8ff92d52cf6f700a3c
Manasi Singh <manasis529@gmail.com>
2026-06-03 16:59:15 +0530

    process env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
+require(dotenv).config()
 const express = require("express");
 const mongoose  = require("mongoose");
 const {courseRouter} = require("./routes/course");
@@ -13,7 +14,7 @@ app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
 
 async function main(){
-   await mongoose.connect("mongodb+srv://manasi123:manu123@cluster1.gkye6zg.mongodb.net/coursera")
+   await mongoose.connect(process.env.MONGO_URL)
    app.listen(3000);
    console.log("listening to port 3000");
 }
